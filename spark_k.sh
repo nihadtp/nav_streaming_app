0c7eefaee4db6a5c0d85e1b5fe16ae87d0ee8e77
Nihad TP <nihadtp@nihad.myfiosgateway.com>
2022-07-18 02:28:25 -0400

    Configuration update

--- spark_k.sh
+++ spark_k.sh
@@ -1,13 +1,15 @@
-"$SPARK_HOME"/bin/spark-submit \
+docker run --name spark -p 9092:9090 -p 4042:4040 nihadtp/mutual-fund-streaming:v1 driver \
 --class org.mutualFund.App \
 --conf spark.driver.extraJavaOptions="-Divy.cache.dir=/tmp -Divy.home=/tmp" \
+--conf spark.executor.instances=3 \
+--conf spark.kubernetes.container.image=nihadtp/mutual-fund-streaming:v1 \
 --packages com.typesafe:config:1.3.4,\
 net.sourceforge.argparse4j:argparse4j:0.9.0,\
 org.apache.kafka:kafka-clients:2.8.0,\
 org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.0,\
 org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 \
---master "$1" \
+--master k8s://https://127.0.0.1:62704 \
  local:///opt/application/prodigal/target/prodigal-1.0-SNAPSHOT.jar \
---config local://prodigal/prodigal_conf.conf;
+--config /opt/application/prodigal/prodigal_conf.conf;
 
 
